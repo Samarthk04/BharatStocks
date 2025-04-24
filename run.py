0dbe5d57447a38f77622a34f6f96a3022fa6bd4a
Samarthk04 <samarthkakkar13@gmail.com>
2025-04-24 15:37:09 +0530

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+# run.py
+from app import create_app, socketio, db
+
+app = create_app()
+
+# Create database tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
